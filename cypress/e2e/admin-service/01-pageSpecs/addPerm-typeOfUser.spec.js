2fe1651df4bc1017adab5599b99dad79e6dd888b
mdolaDFE <Madhu.Dola@education.gov.uk>
2023-06-08 00:53:51 +0100

    FHG-1599,FHG-3386,FHG-1065,FHG-3382

---
+++ cypress/e2e/admin-service/01-pageSpecs/addPerm-typeOfUser.spec.js
@@ -0,0 +1,52 @@
+describe('addPerm-typeOfUser - FHG-1065 DFE - add permissions - who are you adding permissions for? ',()=>{
+    beforeEach(()=> {
+		cy.visit('/')
+        cy.startPage()
+        cy.stubLogin('dfeAdmin')
+	})
+    it('AC 1,5 -  validate content',()=>{
+        cy.welcomePage('Dfe Admin User')
+        cy.addPermissions()
+        cy.title().should('eq','Type of user - Manage family support services and accounts - GOV.UK')
+        cy.pageHeadings().contains('Who are you adding permissions for?')
+        cy.contains('Someone who works for a local authority')
+        cy.contains('Someone who works for a voluntary and community sector organisation')
+
+        cy.typeOfUserPage('la')
+        // back link
+        cy.get('.govuk-back-link').click()
+        cy.contains('Someone who works for a local authority')
+
+    })
+    it('AC 2  -  LA option',()=>{
+        cy.welcomePage('Dfe Admin User')
+        cy.addPermissions()
+        cy.typeOfUserPage('la')
+        cy.typeOfUserLA()
+    })
+    it('AC 3  -  VCS option',()=>{
+        cy.welcomePage('Dfe Admin User')
+        cy.addPermissions()
+        cy.typeOfUserPage('vcs')
+        cy.typeOfUserVCS()
+    })
+    it('AC 4  -  error message',()=>{
+        cy.welcomePage('Dfe Admin User')
+        cy.addPermissions()
+       // error message when user does not select one of the options
+        cy.get('.govuk-button').click()
+        cy.get('.govuk-error-summary').contains('There is a problem')
+        cy.get('.govuk-error-summary').contains('Select who you are adding permissions for')
+        //user makes a seletion
+        cy.typeOfUserPage('la')
+        cy.typeOfUserLA()
+
+    })
+
+})
+describe('User not logged in - access page without logging in - error page',()=>{
+    it('AC 6 - error page - *** not implemented ***',()=>{
+        cy.visit('/AccountAdmin/TypeOfRole')
+    })
+   
+})
