e0b7b2525fcbd76c6cbe339d9580266b3559d9ab
mdolaDFE <Madhu.Dola@education.gov.uk>
2023-08-01 17:05:47 +0100

    Refactor regression

--- cypress/e2e/admin-service/pageSpecs/addPerm-errorPages.spec.js
+++ cypress/e2e/admin-service/pageSpecs/addPerm-errorPages.spec.js
@@ -1,18 +1,23 @@
-describe.skip('| addPerm-errorPages | - all error pages 404,500 ',()=>{
-    it('When user logs in with insufficient credentials - error page - *** not implemented ***',()=>{
+describe('| addPerm-errorPages | - all error pages 404,500 ',()=>{
+    it('When user logs in with insufficient credentials - error page - *** not implemented ***',{ skipBeforeEach: true },()=>{
          cy.visit('/AccountAdmin/TypeOfRole', {failOnStatusCode: false})
 
     })
-    it('error pages - 400 , 500 ,404, error/test',function(){
+    it.only(
+        'error pages - 400 , 500 ,404',
+        { skipBeforeEach: true },
+        ()=>{
+       
         const pages = ['400','500','404']
         for (let i=0; i < pages.length;i++){
+            cy.visit('/')
+            cy.get('.govuk-header__link').contains('Sign out').click()
             cy.visit(`/${pages[i]}`,{failOnStatusCode: false})
-            // cy.visit('/error/test',{failOnStatusCode: false})
-            // cy.contains('Sorry, there is a problem with the service')
-            // cy.contains('Page not found')
-            // cy.contains('If you typed the web address, check it is correct.')
-            // cy.contains('If you pasted the web address, check you copied the entire address.')
-            // cy.contains('If the problem continues, email us at find-support-for-your-family.service@education.gov.uk')
+            
+            cy.contains('Page not found')
+            cy.contains('If you typed the web address, check it is correct.')
+            cy.contains('If you pasted the web address, check you copied the entire address.')
+            cy.contains('If the problem continues, email us at find-support-for-your-family.service@education.gov.uk')
         }
         
     })
---
+++ cypress/e2e/admin-service/pageSpecs/manVCS-addVcsConfirmPage.spec.js
@@ -0,0 +1,20 @@
+describe('| manVCS-addVcsConfirmPage.spec | FHG-3803 Manage VCS - add Organisations ConfirmationPage ',()=>{
+        const num = Date.now();
+        const n = num.toString();
+    
+
+    it('AC 1,2,3  Add VCS Org confirmation page',()=>{
+        cy.visit('/')
+        cy.welcomePage( )
+         //manage VCS link
+        cy.manVcsLink()
+        cy.get('.govuk-pagination__list > :nth-child(2) > .govuk-pagination__link').click()
+        cy.get(':nth-child(5) > .govuk-pagination__link').click()
+        cy.get(':nth-child(2) > .govuk-table__cell--numeric').contains('View').click()
+        cy.editVcsName(n + 'Test VCS Org 001')
+        cy.get('#buttonContinue').click()
+        cy.addVcsOrgConfirm()
+        cy.welcomePage( )
+    })  
+
+})
