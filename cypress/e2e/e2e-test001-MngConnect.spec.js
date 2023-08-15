efaa88e0a2da7e468811cebc75c821678289a734
Harsha Reddy <harsha.reddy@talentconsulting.uk>
2023-08-15 13:58:37 +0100

    Fetch serviceid and pass it to login command

--- cypress/e2e/e2e-test001-MngConnect.spec.js
+++ cypress/e2e/e2e-test001-MngConnect.spec.js
@@ -1,22 +1,23 @@
-describe('| e2e-test001-MngConnect | Manage - add VCS organisation , add la and vcs permissions , Connect - create and approve request , view requests',()=>{
+
+describe('| e2e-test001-MngConnect | Manage - add VCS organisation , add la and vcs permissions , Connect - create and approve request , view requests', () => {
+
     afterEach(() => {
         if (Cypress.mocha.getRunner().suite.ctx.currentTest.state === 'failed') {
             Cypress.runner.stop()
         } 
     })
+
     it('Manage - (dfe admin) Add VCS organisation using spread sheet',()=>{
 		cy.visit('/')
         cy.managelogin('oneloginusername', 'oneloginpassword')
         cy.visit('/Welcome')
         cy.dfeAdminWelcomePage()
         cy.uploadSheet()  
-        // validate if the vcs has been added 
         cy.get('.govuk-header__product-name').click()
         cy.manVcsLink()
         cy.get(':nth-child(4) > .govuk-pagination__link').click()
         cy.contains('Test Harsha Madhu Vcs001')
     })
-	
     
     it('Manage - (dfe admin) Add permissions to user LA Professional', () => {  
         cy.visit('/')
@@ -51,40 +52,29 @@ describe('| e2e-test001-MngConnect | Manage - add VCS organisation , add la and 
     it('Connect - (la Professional) Create and view connection request ',()=>{
         cy.visit('https://test.connect-families-to-support.education.gov.uk');
         cy.refServLanding();
-        cy.searchbypostcode('bs14 8at');
-        //Select the first result on search results page
+        cy.searchbypostcode('E1 5NP');
         cy.get('ul.search-results>li:nth-child(1) a').click();
-        cy.connectlogin('oneloginLAusername', 'oneloginpassword')
-        cy.visit('https://test.connect-families-to-support.education.gov.uk/ProfessionalReferral/Safeguarding?serviceId=277')
+        cy.get('.govuk-grid-column-two-thirds > .govuk-button').each(($el) => {
+            const href = $el.attr('href');
+            const serviceid = href.substring(45)
+            cy.connectlogin('oneloginLAusername', 'oneloginpassword', serviceid);
+            cy.visit(`https://test.connect-families-to-support.education.gov.uk/ProfessionalReferral/Safeguarding?serviceId=${serviceid}`);
+        });
         cy.get('.app-button--inverted').click();
-        //click on Yes radio button and continue on privacy statement page
         cy.selectRadioButtonAndContinue('#shared-privacy-yes', 'div.govuk-grid-row button');
-        //click on Yes radio button and continue on consent page
         cy.selectRadioButtonAndContinue('#consent-yes', 'div.govuk-grid-row button');
-        //enter a contact name and continue on family contact name page
         cy.enterTextAndContinue('.govuk-input', 'James Bond', 'div.govuk-grid-row button');
-        //enter reason and continue 
         cy.reasonForConnectionRequestPage();
-        //select all checkboxes
-        cy.selectCheckBoxes('Email');;
-        //click continue button
+        cy.selectCheckBoxes('Email');
         cy.get('div.govuk-grid-row button').click();
-        //enter a valid email address and continue 
         cy.enterTextAndContinue('.govuk-input', 'a@test.com', 'div.govuk-grid-row button');
-        //click continue button
         cy.get('div.govuk-grid-row button').click();
-        //Enter text in the reason text area
         cy.get('#reason').type('Test service engage with this family');
-        //click continue button
         cy.get('div.govuk-grid-row button').click();
-        //click on Telephone and email radio button
         cy.get('#telephone-and-email').click();
-        //enter a telephone number and continue
         cy.enterTextAndContinue('.govuk-input', '01132 347 902', 'div.govuk-grid-row button');
-        //click Confirm details button
         cy.contains('Confirm details and send request').click();
         cy.checkPanelText('Connection request sent');
-        //cy.contains('Requests sent').click();
     })
 
     // session details change - sign in as LA Pro = user id = harshareddy.leeds@googlemail.com
@@ -106,8 +96,6 @@ describe('| e2e-test001-MngConnect | Manage - add VCS organisation , add la and 
         cy.deletePermissionsLink()
         cy.deletePermissionsOptionPage('TH - LA Pro','Yes')
         cy.deletePermissionsConfirmPage('TH - LA Pro')
-       
-
     })
 
     it('Manage - (dfe admin) delete VCS organisation',()=>{
@@ -115,14 +103,11 @@ describe('| e2e-test001-MngConnect | Manage - add VCS organisation , add la and 
         cy.managelogin('oneloginusername', 'oneloginpassword')
         cy.visit('/Welcome')
         cy.dfeAdminWelcomePage()
-         //manage VCS link
         cy.manVcsLink()
         cy.get('.govuk-pagination__list li:last-child').click();
         cy.get('td.govuk-table__cell a[data-testid="delete_TestHarshaMadhuVcs001"]').click();
         cy.contains('Deleting an organisation');
         cy.selectRadioButtonAndContinue('#removeOrg', 'button.govuk-button')
         cy.contains('You have deleted Test Harsha Madhu Vcs001')
-
-       
 	})
 })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -372,12 +372,12 @@ Cypress.Commands.add('managelogin', (olusername, olpassword) => {
     })
 })
 
-Cypress.Commands.add('connectlogin', (olusername, olpassword) => {
+Cypress.Commands.add('connectlogin', (olusername, olpassword, id) => {
     cy.session([olusername, olpassword], () => {
 
         cy.visit(`https://${Cypress.env('username')}:${Cypress.env('password')}@signin.integration.account.gov.uk/?prompt=login`, { failOnStatusCode: false })
         //Click on Request a connection button
-        cy.visit('https://test.connect-families-to-support.education.gov.uk/ProfessionalReferral/LocalOfferDetail?serviceid=277')
+        cy.visit(`https://test.connect-families-to-support.education.gov.uk/ProfessionalReferral/LocalOfferDetail?serviceid=${id}`)
         cy.get('a:contains("Request a connection")').click();
         //stub-login
         cy.get('#sign-in-button').click()
@@ -391,22 +391,6 @@ Cypress.Commands.add('connectlogin', (olusername, olpassword) => {
     })
 })
 
-Cypress.Commands.add('dashboardlogin', (olusername, olpassword, dashboardType) => {
-    cy.session([olusername, olpassword], () => {
-
-        cy.visit(`https://${Cypress.env('username')}:${Cypress.env('password')}@signin.integration.account.gov.uk/?prompt=login`, { failOnStatusCode: false })
-        //Click on Request a connection button
-        cy.visit(`https://test.manage-connection-requests.education.gov.uk/${dasboardType}/Dashboard`)
-        cy.get('#sign-in-button').click()
-        // login based on type of user 
-        // login email
-        cy.get('#email').type(`${Cypress.env(olusername)}`)
-        cy.get('form > .govuk-button').click()
-        // login password
-        cy.get('#password').type(`${Cypress.env(olpassword)}`)
-        cy.get('form > .govuk-button').click()
-    })
-})
 // Sort ascending / descending 
 Cypress.Commands.add('checkSortOrder', (value, sortOrder)=> {
     cy.get('th.govuk-table__header').eq(value).invoke('attr', 'aria-sort').then((ariasort) => {
