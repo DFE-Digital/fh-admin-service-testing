8090090a38dd9fad133dc3811dfaa9ba1555a409
Witek <witek.poreda@education.gov.uk>
2023-08-18 14:13:41 +0100

    fix manVcs-dfeAmin-delOrgPage tests

--- cypress/e2e/laManager/manageVCSOrgs/manVcs-LAMan-delOrgPage.spec.js
+++ cypress/e2e/laManager/manageVCSOrgs/manVcs-LAMan-delOrgPage.spec.js
@@ -1,58 +1,51 @@
 describe('| manVcs-dfeAmin-delOrgPage | , FHG-3805 DFE Admin -Manage VCS organisations (Deleting an organisation page)',{ tags: ['LAMan'] },()=>{
-  
-    it('AC 1,5,6 - page content , back link , title ',function(){
+
+    beforeEach(() => {
         cy.visit('/')
         cy.integrationLogin('laman')
         cy.LAManWelcomePage('London Borough of Redbridge')
-         //manage VCS link
-        cy.manVcsLink()
+    });
+        
+    it('AC 1,5,6 - page content , back link , title ',function(){
+        //manage VCS link
+        cy.laManVcsLink()
         cy.get(':nth-child(4) > .govuk-pagination__link').click()
         cy.manVcsDel()
-        cy.DelVcsPage()    
+        cy.DelVcsPage()
         // back link
         cy.get('.govuk-back-link').click()
-       cy.contains('Manage organisations')
+        cy.contains('Manage organisations')
     
-})
-    it('AC 2 - Yes, I want to delete it',function(){
-        cy.visit('/')
-        cy.LAManWelcomePage('London Borough of Redbridge')
-         //manage VCS link
-        cy.manVcsLink()
-        cy.get(':nth-child(4) > .govuk-pagination__link').click()
-        cy.manVcsDel()
-        cy.DelVcsPage('Yes')    
-        // add validation - You have deleted the service confirmation page
-        cy.contains('')
     })
-     it('AC 3 - No, I want to keep it ,',function(){
-        cy.visit('/')
-        cy.LAManWelcomePage('London Borough of Redbridge')
+    it('AC 2 - Yes, I want to delete it',function(){
          //manage VCS link
-        cy.manVcsLink()
+        cy.laManVcsLink()
         cy.get(':nth-child(4) > .govuk-pagination__link').click()
         cy.manVcsDel()
-        cy.DelVcsPage('No')    
+        cy.DelVcsPage('Yes')
+        // add validation - You have deleted the service confirmation page
+        cy.contains('You have deleted')
+    })
+    it('AC 3 - No, I want to keep it ,',function(){
+         //manage VCS link
+        cy.laManVcsLink()
+        cy.get(':nth-child(4) > .govuk-pagination__link').click()
+        cy.manVcsDel()
+        cy.DelVcsPage('No')
         // add validation - You have not deleted the service confirmation page
         cy.VcsNotDelPage()
     })
-    it.only('AC 4 - error messages  ,',function(){
-        cy.visit('/')
-        cy.LAManWelcomePage('London Borough of Redbridge')
+    it('AC 4 - error messages  ,',function(){
          //manage VCS link
-        cy.manVcsLink()
+        cy.laManVcsLink()
         cy.get(':nth-child(4) > .govuk-pagination__link').click()
         cy.manVcsDel()
-        cy.DelVcsPage()   
+        cy.DelVcsPage()
         // error message when user does not select one of the options
         cy.get('.govuk-button').click()
         cy.get('.govuk-error-summary').contains('There is a problem')
         cy.get('.govuk-error-summary').contains('Select if you want to delete the organisation') 
         // make a selection
-        cy.DelVcsPage('No')    
-       
+        cy.DelVcsPage('No')
     })
-
-
-
 })
