31dde12ecea13ce38b81ebf6b9382a823352ea13
Witek <witek.poreda@education.gov.uk>
2023-09-12 11:50:42 +0100

    fix mobile tests

--- cypress/e2e/dfeAdmin/mobile-e2e-journeys/dfeAdmin-addPerm-vcs-e2ejourney-mobile.spec.js
+++ cypress/e2e/dfeAdmin/mobile-e2e-journeys/dfeAdmin-addPerm-vcs-e2ejourney-mobile.spec.js
@@ -1,95 +1,54 @@
-describe('| 📱 addPerm-vcs-e2ejourney-mobile | ',{tags: ['dfeAdmin']},()=>{
-        const num = Date.now();
-        const n = num.toString();
-         beforeEach(()=> {
-		cy.visit('/')
-        cy.integrationLogin('dfeadmin')   
-	})
-    
-    it(`iphone-x - VCS Journey - Validate mobile version`,function(){
+describe('| 📱 addPerm-vcs-e2ejourney-mobile | ', { tags: ['dfeAdmin'] }, () => {
+    const num = Date.now();
+    const n = num.toString();
+    beforeEach(() => {
+        cy.visit('/')
+        cy.integrationLogin('dfeadmin')
+    })
+
+    it(`iphone-x - VCS Journey - Validate mobile version`, function () {
         cy.viewport(`iphone-x`)
         cy.visit('/')
-       // cy.startPage()
-        // cy.stubLogin('dfeAdmin')
-        cy.myaccountMob()
         cy.dfeAdminWelcomePage()
-        cy.myaccountMob()
         cy.addPermissions()
-        cy.myaccountMob()
         cy.typeOfUserPage('vcs')
-        cy.myaccountMob()
         cy.typeOfUserVCS('1')
-        cy.myaccountMob()
         cy.vcsWhichLA('redbridge')
-        cy.myaccountMob()
         cy.whichOrgVcs('cranbrook')
-        cy.myaccountMob()
-        cy.email(n +'abcdef@def.com')
-        cy.myaccountMob()
+        cy.email(n + 'abcdef@def.com')
         cy.fullName('John Paul Smith')
-        cy.myaccountMob()
         cy.checkAnswerPage()
-        cy.myaccountMob()
         cy.confirmationPage('John Paul Smith')
-        cy.myaccountMob()
-    
     })
-     it(`samsung-s10 - VCS Journey - Validate mobile version`,function(){
+
+    it(`samsung-s10 - VCS Journey - Validate mobile version`, function () {
         cy.viewport(`samsung-s10`)
         cy.visit('/')
-       // cy.startPage()
-        // cy.stubLogin('dfeAdmin')
-        cy.myaccountMob()
         cy.dfeAdminWelcomePage()
-        cy.myaccountMob()
         cy.addPermissions()
-        cy.myaccountMob()
         cy.typeOfUserPage('vcs')
-        cy.myaccountMob()
         cy.typeOfUserVCS('1')
-        cy.myaccountMob()
         cy.vcsWhichLA('redbridge')
-        cy.myaccountMob()
         cy.whichOrgVcs('cranbrook')
-        cy.myaccountMob()
-        cy.email(n +'+abcdef@def.com')
-        cy.myaccountMob()
+        cy.email(n + '+abcdef@def.com')
         cy.fullName('+John Paul Smith')
-        cy.myaccountMob()
         cy.checkAnswerPage()
-        cy.myaccountMob()
         cy.confirmationPage('+John Paul Smith')
-        cy.myaccountMob()
-    
     })
-    //
-     it(`ipad-mini - VCS Journey - Validate mobile version`,function(){
+
+    it(`ipad-mini - VCS Journey - Validate mobile version`, function () {
         cy.viewport(`ipad-mini`)
         cy.visit('/')
-       // cy.startPage()
-        // cy.stubLogin('dfeAdmin')
-        cy.myaccountMob()
         cy.dfeAdminWelcomePage()
-        cy.myaccountMob()
         cy.addPermissions()
-        cy.myaccountMob()
         cy.typeOfUserPage('vcs')
-        cy.myaccountMob()
         cy.typeOfUserVCS('1')
-        cy.myaccountMob()
         cy.vcsWhichLA('redbridge')
-        cy.myaccountMob()
         cy.whichOrgVcs('cranbrook')
-        cy.myaccountMob()
-        cy.email(n +'++abcdef@def.com')
-        cy.myaccountMob()
+        cy.email(n + '++abcdef@def.com')
         cy.fullName('++John Paul Smith')
-        cy.myaccountMob()
         cy.checkAnswerPage()
-        cy.myaccountMob()
         cy.confirmationPage('++John Paul Smith')
-        cy.myaccountMob()
-    
     })
-    
+
 })
