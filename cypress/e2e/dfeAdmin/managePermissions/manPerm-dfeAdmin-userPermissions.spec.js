19ce96b77ca7ed8e32aeebe0feb2c4181f006bbc
Witek <witek.poreda@education.gov.uk>
2023-09-12 10:21:39 +0100

    fix manage permissions tests

--- cypress/e2e/dfeAdmin/managePermissions/manPerm-dfeAdmin-userPermissions.spec.js
+++ cypress/e2e/dfeAdmin/managePermissions/manPerm-dfeAdmin-userPermissions.spec.js
@@ -7,10 +7,10 @@ describe('| manPerm-userPermissions | FHG-1617 Manage permissions ( Manage User 
     it('AC 1 ', function () {
         cy.visit('/')
         //my account link
-        cy.get('.govuk-header__content').contains('My account').click()
+        cy.get('.govuk-link--inverse').contains('My account').click()
         cy.contains('Manage your account')
         //signout link
-        cy.get('.govuk-header__content').contains('Sign out').click()
+        cy.get('.govuk-link--inverse').contains('Sign out').click()
         cy.contains('You have signed out')
     })
 })
