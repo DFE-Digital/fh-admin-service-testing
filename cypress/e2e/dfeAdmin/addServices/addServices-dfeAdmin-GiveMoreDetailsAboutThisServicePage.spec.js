3feb7defb342fb36d2261b8346422724c24c9446
Harsha Reddy <harsha.reddy@talentconsulting.uk>
2024-03-25 09:05:42 +0000

    Add service journey tests

---
+++ cypress/e2e/dfeAdmin/addServices/addServices-dfeAdmin-GiveMoreDetailsAboutThisServicePage.spec.js
@@ -0,0 +1,143 @@
+﻿describe('DfE Admin - Add services - Give more details about this service page', () => {
+    beforeEach(() => {
+        const checkboxList = ['Activities, clubs and groups', 'Holiday clubs and schemes'];
+        cy.visit('/')
+        cy.integrationLogin('dfeadmin')
+        cy.contains('Add a service').click();
+        //click on continue button
+        cy.get('div.govuk-grid-row button').click();
+        //enter a service name
+        cy.enterTextAndContinue('.govuk-input', 'test service', 'div.govuk-grid-row button');
+        //Select a category
+        checkboxList.forEach((checkbox) => {
+            cy.selectCheckBoxes(checkbox);
+        })
+        //click on continue button
+        cy.get('div.govuk-grid-row button').click();
+        //Enter text in the reason text area
+        cy.get('#textarea').type('Test description');
+        //click on continue button
+        cy.get('div.govuk-grid-row button').click();
+        //select No radio button
+        cy.selectRadioButtonAndContinue('#ViewModel_Children_No', 'div.govuk-grid-row button');
+        //Select a language
+        cy.selectLanguage('#language-0', '#language-0__option--0', 'Fre');
+        //click continue button 
+        cy.get('form > :nth-child(5)').click();
+        //select No radio button 
+        cy.get('#UserInput_HasCost_No').click();
+        //click on continue button
+        cy.get('div.govuk-grid-row button').click();
+        //select In person checkbox
+        cy.selectCheckBoxes('In person');
+        //click on continue button
+        cy.get('div.govuk-grid-row button').click();
+        //select No radio button
+        cy.get('#radio-False').click();
+        //click on continue button
+        cy.get('div.govuk-grid-row button').click();
+        //click on continue button
+        cy.get('div.govuk-grid-row button').click();
+        //select No radio button
+        cy.get('#UserInput_HasDetails_No').check();
+        //click on continue button
+        cy.get('div.govuk-grid-row button').click();
+        //select Email checkbox
+        cy.selectCheckBoxes('Email');
+        //enter email address
+        cy.enterText('#email-text-box', 'a@test.com');
+        //click on continue button
+        cy.get('div.govuk-grid-row button').click();
+    })
+
+    it('validate page content, checkboxes and back link', () => {
+        const expectedPageHeading = "Give more details about this service";
+        const expectedStaticText = 'Tell people anything else they need to about using this service, such as:';
+        const expectListText = ['different age ranges for people whith special educational needs and disabilities ( SEND )',
+            'if they need to book and how to do this', 'any accessibility support offered'];
+        let actualList = [];
+        const expectedPrevPageHeading = 'How can people find out more about this service?';
+        const expectedHintText = 'You have 500 characters remaining';
+
+        //check page heading
+        cy.checkPageHeading("h1", expectedPageHeading);
+        //check static text
+        cy.checkTextOf('.govuk-grid-column-two-thirds > p', expectedStaticText);
+        //check text on the page
+        cy.getTextOfElements('.govuk-list > li', actualList, expectListText);
+        //check text area content
+        cy.checkTextOf('#textarea', '');
+        //check character limit text
+        cy.checkTextOf('.govuk-hint:not(.govuk-visually-hidden)', expectedHintText);
+        //Click on back link
+        cy.clickBackLink();
+        //verify page heading
+        cy.checkPageHeading('.govuk-label-wrapper > .govuk-label', expectedPrevPageHeading);
+    })
+
+    it('should save the text when navigated to next page', () => {
+        const expectedHeading = 'Check the details and add service';
+        const initialHintText = 'You have 500 characters remaining';
+        const finalHintText = 'You have 483 characters remaining';
+
+        //check hint text for character count with blank text box
+        cy.checkTextOf('.govuk-hint:not(.govuk-visually-hidden)', initialHintText);
+        //Enter text in the reason text area
+        cy.get('#textarea').type('Test more details');
+        //check hint text for character count after entering the text
+        cy.checkTextOf('.govuk-hint:not(.govuk-visually-hidden)', finalHintText);
+        //click continue button 
+        cy.get('div.govuk-grid-row button').click();
+        //check page heading
+        cy.checkPageHeading('h1', expectedHeading);
+        //Click on back link
+        cy.clickBackLink();
+        //check saved text
+        cy.checkTextOf('#textarea', 'Test more details');
+    })
+
+    it('Navigate to next page with out entering more details', () => {
+        const expectedHeading = 'Check the details and add service';
+        const initialHintText = 'You have 500 characters remaining';
+
+        //check hint text for character count with blank text box
+        cy.checkTextOf('.govuk-hint:not(.govuk-visually-hidden)', initialHintText);
+        //click continue button 
+        cy.get('div.govuk-grid-row button').click();
+        //check page heading
+        cy.checkPageHeading('h1', expectedHeading);
+        //Click on back link
+        cy.clickBackLink();
+        //check saved text
+        cy.checkTextOf('#textarea', '');
+    })
+
+    it('display error messages for exceeded character count', () => {
+        const enteredText = 'Test more details about this service.'.repeat(20);
+        const expectedHintText = 'You have 240 characters too many';
+        const errorHeading = 'There is a problem';
+        const errorMessage = 'More details must be 500 characters or less';
+        const expectedHeading = 'Check the details and add service';
+        let [actualBannerMessages, actualMessages] = [[], []];
+
+        //Enter text in the reason text area
+        cy.get('#textarea').type(enteredText);
+        //check hint text for character count
+        cy.checkTextOf('.govuk-character-count__message:not(.govuk-visually-hidden)', expectedHintText);
+        //click continue button 
+        cy.get('div.govuk-grid-row button').click();
+        //check error messages
+        cy.get('.govuk-error-summary__title').should('contain', errorHeading);
+        cy.get('.govuk-list > li > a').should('have.text', errorMessage);
+        cy.get('#textarea-error-message').should('contain', errorMessage);
+        //check hint text for character count after error message
+        cy.checkTextOf('.govuk-character-count__message:not(.govuk-visually-hidden)', expectedHintText);
+        //Enter text in the reason text area
+        cy.get('#textarea').clear().type('Test description');
+        //click continue button 
+        cy.get('div.govuk-grid-row button').click();
+        //check page heading
+        cy.checkPageHeading('h1', expectedHeading);
+    })
+
+})
---
+++ cypress/e2e/dfeAdmin/addServices/addServices-dfeAdmin-SearchAndSelectALocationPage.spec.js
@@ -0,0 +1,147 @@
+﻿describe('DfE Admin - Add services - Search and select a location page', () => {
+    var n = getDateString();
+    const addressline1 = 'Test address' + n;
+
+    beforeEach(() => {
+        const checkboxList = ['Activities, clubs and groups', 'Holiday clubs and schemes'];
+        cy.visit('/')
+        cy.integrationLogin('dfeadmin')
+        cy.contains('Add a service').click();
+        //click on continue button
+        cy.get('div.govuk-grid-row button').click();
+        //enter a service name
+        cy.enterTextAndContinue('.govuk-input', 'test service', 'div.govuk-grid-row button');
+        //Select a category
+        checkboxList.forEach((checkbox) => {
+            cy.selectCheckBoxes(checkbox);
+        })
+        //click on continue button
+        cy.get('div.govuk-grid-row button').click();
+        //Enter text in the reason text area
+        cy.get('#textarea').type('Test description');
+        //click on continue button
+        cy.get('div.govuk-grid-row button').click();
+        //select No radio button
+        cy.selectRadioButtonAndContinue('#ViewModel_Children_No', 'div.govuk-grid-row button');
+        //Select a language
+        cy.selectLanguage('#language-0', '#language-0__option--0', 'Fre');
+        //click continue button 
+        cy.get('form > :nth-child(5)').click();
+        //select No radio button 
+        cy.get('#UserInput_HasCost_No').click();
+        //click on continue button
+        cy.get('div.govuk-grid-row button').click();
+        //select In person checkbox
+        cy.selectCheckBoxes('In person');
+        //click on continue button
+        cy.get('div.govuk-grid-row button').click();
+        //select Yes radio button
+        cy.get('#radio-True').click();
+        //click on continue button
+        cy.get('div.govuk-grid-row button').click();
+    })
+
+    it('validate page content and back link', () => {
+        const expectedPageHeading = "Search and select a location to add to this service";
+        const expectedStaticText = 'If the location does not appear here you can add a new location.';
+        const expectedPrevPageHeading = 'Do you want to add any locations for this service?';
+
+        //check page heading
+        cy.checkPageHeading("h1", expectedPageHeading);
+        //check hint text
+        cy.checkTextOf('main#main-content p', expectedStaticText);
+        //check add location href
+        cy.get('form > p > a').should('have.attr', 'href', '/manage-locations/start-add-location?journey=Service');
+        //Click on back link
+        cy.clickBackLink();
+        //verify page heading
+        cy.checkPageHeading('h1', expectedPrevPageHeading);
+    })
+
+    it('Search a postcode, select the location and continue to next page', () => {
+        const expectedNextPageHeading = "On which days can people use this service at The Salvation Army?";
+        const expectedPageHeading = 'Search and select a location to add to this service';
+        const expectedLocation = 'The Salvation Army, 15\nClements Road, Ilford, Essex, IG1 1BH';
+
+        //enter postcode and continue
+        cy.enterText('#select-location-location', 'IG1 1BH');
+        //click on location
+        cy.get('#select-location-location__option--0').click();
+        //click on continue button
+        cy.get('div.govuk-grid-row button').click();
+        //check next page heading
+        cy.checkPageHeading('h1', expectedNextPageHeading);
+        //Click on back link
+        cy.clickBackLink();
+        //verify page heading
+        cy.checkPageHeading('h1', expectedPageHeading);
+        //check saved location
+        cy.get('#select-location-location-select').invoke('val').then(value => {
+            const selectedText = Cypress.$(`#select-location-location-select option[value="${value}"]`).text();
+            expect(selectedText).to.equal(expectedLocation);
+        });
+    })
+
+    it('Search by partial address, select the location and continue to next page', () => {
+        const expectedNextPageHeading = "On which days can people use this service at The Salvation Army?";
+        const expectedPageHeading = 'Search and select a location to add to this service';
+        const expectedLocation = 'The Salvation Army, 15\nClements Road, Ilford, Essex, IG1 1BH';
+
+        //enter postcode and continue
+        cy.enterText('#select-location-location', 'The Salva');
+        //click on location
+        cy.get('#select-location-location__option--0').click();
+        //click on continue button
+        cy.get('div.govuk-grid-row button').click();
+        //check next page heading
+        cy.checkPageHeading('h1', expectedNextPageHeading);
+        //Click on back link
+        cy.clickBackLink();
+        //verify page heading
+        cy.checkPageHeading('h1', expectedPageHeading);
+        //check saved location
+        cy.get('#select-location-location-select').invoke('val').then(value => {
+            const selectedText = Cypress.$(`#select-location-location-select option[value="${value}"]`).text();
+            expect(selectedText).to.equal(expectedLocation);
+        });
+    })
+
+    it('add a new location on the search locations page', () => {
+        const expectedPageHeading = "Search and select a location to add to this service";
+        const enteredAddress = new Map([
+            ['buildingName', addressline1],
+            ['line1', 'Test address 1'],
+            ['line2', 'Street 2'],
+            ['townOrCity', 'Bristol'],
+            ['county', 'Bristol county'],
+            ['postcode', 'LS9 8NQ']
+        ]);
+        const expectedLocation = addressline1+", Test address 1, Street 2, Bristol, Bristol county, LS9 8NQ";
+
+        //click on add a new location link
+        cy.contains('add a new location').click();
+        //enter a valid address and continue 
+        enteredAddress.forEach((text, id) => {
+            cy.enterText(`#${id}`, text);
+        })
+        //click on continue button
+        cy.get('div.govuk-grid-row button').click();
+        //Select No radio button and continue
+        cy.selectRadioButtonAndContinue('#radio-False', 'div.govuk-grid-row button');
+        //Enter text in the text area
+        cy.get('#textarea').type('Test more details about the service');
+        //click continue button 
+        cy.get('div.govuk-grid-row button').click();
+        //click continue button 
+        cy.get('div.govuk-grid-row button').click();
+        //check page heading
+        cy.checkPageHeading("h1", expectedPageHeading);
+        //check new location added
+        cy.get('#select-location-location-select').invoke('val').then(value => {
+            const selectedText = Cypress.$(`#select-location-location-select option[value="${value}"]`).text();
+            expect(selectedText).to.equal(expectedLocation);
+        });
+
+    })
+
+})
